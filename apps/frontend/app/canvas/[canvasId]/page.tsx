de7a89b7b2f7eacbd289883fc5667e928e6a90ea
anish877 <62011810+anish877@users.noreply.github.com>
2025-02-10 12:48:50 +0530

    rect drawing

--- apps/frontend/app/canvas/[canvasId]/page.tsx
+++ apps/frontend/app/canvas/[canvasId]/page.tsx
@@ -1,9 +1,10 @@
 'use client';
 import { initDraw } from '@/app/draw';
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 const Canvas = () => {
     const canvasref = useRef<HTMLCanvasElement>(null)
+    const [type,setType] = useState("rect")
     useEffect(()=>{
         const canvas = canvasref.current
         if(!canvas)
@@ -12,7 +13,13 @@ const Canvas = () => {
     },[canvasref])
   return (
     <div >
-      <canvas ref={canvasref} height={800} width={2000} className='bg-white'></canvas>
+        <div>
+            <button onClick={()=>setType("circle")}>Circle</button>
+            <button onClick={()=>setType("rect")}>Rect</button>
+        </div>
+        <div>
+            <canvas ref={canvasref} height={800} width={2000} className='bg-white'></canvas>
+        </div>
     </div>
   )
 }
--- apps/frontend/app/draw/index.ts
+++ apps/frontend/app/draw/index.ts
@@ -1,9 +1,27 @@
+type Shape = {
+    type: "rect",
+    x: number,
+    y: number,
+    width: number,
+    height: number,
+} | {
+    type: "circle",
+    centerx: number,
+    centery: number,
+    radius: number,
+}
+
+
 export function initDraw(canvas:HTMLCanvasElement){
-    let clicked = false
+        let clicked = false
         let startX = 0
         let startY = 0
+        let width = 0
+        let height = 0
         let ctx = canvas.getContext("2d")
-
+        let existingShapes : Shape[] = []
+        if(!ctx)
+            return
         canvas.addEventListener("mousedown",(e)=>{
             clicked = true
             startX = e.clientX
@@ -12,14 +30,29 @@ export function initDraw(canvas:HTMLCanvasElement){
         })
         canvas.addEventListener("mouseup",(e)=>{
             clicked = false
+            existingShapes.push({
+                type: "rect",
+                x: startX,
+                y: startY,
+                width: e.clientX-startX,
+                height: e.clientY-startY
+            })
         })
         canvas.addEventListener("mousemove",(e)=>{
             if(clicked){
-                const width = e.clientX-startX
-                const height = e.clientY - startY
-                console.log(width,height)
-                ctx?.clearRect(0,0,canvas.width||0,canvas.height||0)
+                width = e.clientX-startX
+                height = e.clientY - startY
+                clearCanvas(existingShapes,canvas,ctx)
                 ctx?.strokeRect(startX,startY,width,height)
             }
         })
+}
+
+export function clearCanvas(existingShapes: Shape[], canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D ){
+    ctx.clearRect(0,0,canvas.width,canvas.height)
+    existingShapes.map(shape=>{
+        if(shape.type==="rect"){
+            ctx.strokeRect(shape.x,shape.y,shape.width,shape.height)
+        }
+    })
 }
