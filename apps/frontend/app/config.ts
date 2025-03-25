7d8bbb68177db9df3b487103f07a6146b41954c3
anish877 <62011810+anish877@users.noreply.github.com>
2025-03-25 15:09:39 +0530

    deployed

--- apps/frontend/app/config.ts
+++ apps/frontend/app/config.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "http://localhost:3001"
-export const WS_URL = "ws://localhost:8080"
+export const BACKEND_URL = "https://multiplayer-draw-app-frontend-qztu.vercel.app"
+export const WS_URL = "https://multiplayer-draw-app-frontend-39ww.vercel.app"
