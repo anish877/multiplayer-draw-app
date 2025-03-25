4ba5b806568781a9d4d2a14510bdd76ccbfe77dd
anish877 <62011810+anish877@users.noreply.github.com>
2025-03-25 21:30:20 +0530

    push

--- apps/frontend/app/config.ts
+++ apps/frontend/app/config.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "https://multiplayer-draw-app-frontend-39ww.vercel.app"
-export const WS_URL = "https://multiplayer-draw-app-frontend-39ww.vercel.app"
+export const BACKEND_URL = "https://multiplayer-draw-app-frontend-qztu.vercel.app/"
+export const WS_URL = "https://multiplayer-draw-app-frontend-39ww.vercel.app/"
