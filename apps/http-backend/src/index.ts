4068563d2ae6441309b8d3b116fb467126facdbc
anish877 <62011810+anish877@users.noreply.github.com>
2025-03-25 19:58:11 +0530

    push

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -12,14 +12,14 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: 'https://multiplayer-draw-app-frontend.vercel.app',
+  origin: 'https://multiplayer-draw-app-frontend.vercel.app/',
   credentials: true,
   methods: "GET,POST,PUT,DELETE,OPTIONS",
   allowedHeaders: "Content-Type,Authorization",
 }));
 
 app.options("*", (req, res) => {
-  res.header("Access-Control-Allow-Origin", "https://multiplayer-draw-app-frontend.vercel.app");
+  res.header("Access-Control-Allow-Origin", "https://multiplayer-draw-app-frontend.vercel.app/");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", "true");
